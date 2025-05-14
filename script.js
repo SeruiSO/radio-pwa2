d08ef9205177af4bd2ae2fc64b0e40610f42bea6
SeruiSO <Olars.m.1@gmail.com>
2025-05-14 23:02:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -68,11 +68,6 @@ function applyTheme(theme) {
     el.style.borderColor = themes[theme].text;
     el.style.color = themes[theme].text;
   });
-  document.querySelectorAll(".station-item:hover, .station-item.selected").forEach(el => {
-    el.style.background = themes[theme].accent + " !important";
-    el.style.borderColor = themes[theme].accent + " !important";
-    el.style.color = themes[theme].bodyBg + " !important";
-  });
   document.querySelector(".controls-container").style.background = themes[theme].containerBg;
   document.querySelector(".controls-container").style.borderColor = themes[theme].accent;
   document.querySelector(".volume-slider input").style.background = themes[theme].accent;
